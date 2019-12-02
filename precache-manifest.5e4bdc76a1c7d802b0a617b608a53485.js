43fee5f138b59ef278d6e0b415380dae1c7f0549
Cedric BOUVE <cedric.bouve@actia.fr>
2019-12-02 13:51:16 +0100

    Updates

---
+++ precache-manifest.5e4bdc76a1c7d802b0a617b608a53485.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd3626b91a45d04f46adb8d7dc79c46",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "522d3b131f678b6e1a84",
+    "url": "/monsters-rolodex/static/css/main.f795fac7.chunk.css"
+  },
+  {
+    "revision": "8141f0803164906a6a51",
+    "url": "/monsters-rolodex/static/js/2.fd9aaf2d.chunk.js"
+  },
+  {
+    "revision": "522d3b131f678b6e1a84",
+    "url": "/monsters-rolodex/static/js/main.54e3f183.chunk.js"
+  },
+  {
+    "revision": "82d6f8080f7412f410fc",
+    "url": "/monsters-rolodex/static/js/runtime-main.b56caa74.js"
+  }
+]);
